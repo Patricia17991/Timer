46f6334cc7b227a45331da6e6102598a3aeee3d2
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-17 20:01:35 -0300

    Update Button.styles.ts

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -18,8 +18,12 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainer>`
      width: 100px;
      height: 40px;
+     border-radius: 4px;
+     border: 0;
+     margin: 8px;
 
      background-color: ${props => props.theme.primary};
+     color: ${props => props.theme.blue};
 
    /* ${props => {
         return css`background-color: ${buttonVariants[props.variant]}`
