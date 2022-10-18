e620775b2248a068679b68ad44fdcfc00544e209
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-18 07:54:40 -0300

    Update global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,14 +2,19 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
    * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+     margin: 0;
+     padding: 0;
+     box-sizing: border-box;
    }
 
    body {
-    background: #333;
-    color: #fff;
-    
+     background: #333;
+     color: #fff;
+   }
+
+   border-style, input-security, textarea, button {
+     font-family: 'Roboto', sans-serif;
+     font-weight: 400;
+     font-size: 1rem;
    }
 `;
