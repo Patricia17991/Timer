c3b4ac9bbb96976cd0e9c193523b7f7e8bf4e4d4
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-18 09:20:11 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   )
