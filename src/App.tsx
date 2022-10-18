fd6a8603d35572c3f0a9a17f341a9271f5740d3d
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-18 07:46:11 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Button } from './components/Button';
 import {ThemeProvider} from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
    return(
@@ -9,9 +10,13 @@ export function App() {
     <Button variant="secondary"/>
     <Button variant="success"/>
     <Button variant="danger"/>
+    <GlobalStyle/>
     </ThemeProvider>
   )
 }
+
+
+
 
 
 
