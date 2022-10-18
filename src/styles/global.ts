7341ba665daa73e0728ce58705c712d83b05f483
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-18 09:39:45 -0300

    Update global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,21 +1,25 @@
-export const defaultTheme = {
-    white: '#FFF',
+import { createGlobalStyle } from 'styled-components'
 
-    'gray-100': '#E1E1E6',
-    'gray-300': '#C4C4CC',
-    'gray-400': '#8D8D99',
-    'gray-500': '#7C7C8A',
-    'gray-600': '#323238',
-    'gray-700': '#29292E',
-    'gray-800': '#202024',
-    'gray-900': '#121214',
-  
-    'green-300': '#00B37E',
-    'green-500': '#00875F',
-    'green-700': '#015F43',
-  
-    'red-500': '#AB222E',
-    'red-700': '#7A1921',
-  
-    'yellow-500': '#FBA94C',
-}
+export const GlobalStyle = createGlobalStyle`
+   * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+   :focus {
+     outline: 0;
+     box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+   }
+
+   body {
+     background: ${(props) => props.theme['gray-900']};
+     color: ${(props) => props.theme['gray-300']};
+   }
+
+   border-style, input-security, textarea, button {
+     font-family: 'Roboto', sans-serif;
+     font-weight: 400;
+     font-size: 1rem;
+   }
+`
