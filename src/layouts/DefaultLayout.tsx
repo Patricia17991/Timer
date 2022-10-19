f9ae7b9090aafe355ecf2d8bb34129e9e2d3aa58
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-19 09:18:02 -0300

    Update DefaultLayout.tsx

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,10 +1,11 @@
+import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
 
 export function DefaultLayout() {
     return (
        <div>
          <Header />
-         Conteúdo
+         <Outlet />
        </div>
     )
 }
