36387fec75f14ddc6b80e75e136475814038d6be
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-19 10:12:56 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,10 @@
 import { HeaderContainer } from './styles'
+import logoIgnite from '../../assets/Logo-ignite.svg'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <span>logo</span>
+      <img src={logoIgnite} alt="" />
       <nav>
         <a href="">Timer</a>
         <a href="">History</a>
