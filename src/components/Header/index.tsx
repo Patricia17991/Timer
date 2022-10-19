3fa6c5ab9dc5af8f5ee420718376a645d45005cb
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-19 10:00:52 -0300

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { HeaderContainer } from './styles'
 export function Header() {
   return (
     <HeaderContainer>
+      <span>logo</span>
       <nav>
         <a href="">Timer</a>
         <a href="">History</a>
