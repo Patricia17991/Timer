9da76550c5204fea7ba9ea0e6e6cc35e869c16d1
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-20 12:15:30 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,13 +4,20 @@ import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, S
 export function Home() {
   return (
     <HomeContainer>
-        <form action="">
+      <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput id="task"  placeholder="Dê um nome para o seu projeto" />
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" />
+          <MinutesAmountInput 
+           type="number" 
+           id="minutesAmount" 
+           placeholder="00"
+           step={5}
+           min={5}
+           max={60}
+          />
           <span>minutos.</span>
         </FormContainer>
             
