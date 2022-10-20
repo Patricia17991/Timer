39a31a148330a2246c4d3be9da82906f8a7c2a49
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-20 11:13:36 -0300

    Update styles.ts

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -30,11 +30,22 @@ export const FormContainer = styled.div`
 
 const BaseInput = styled.input`
   background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
 `
 
-export const TaskInput = styled(BaseInput)``
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`
 
-export const MinutesAmountInput = styled(BaseInput)``
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
 
 export const CountdownContainer = styled.div`
   font-family: 'Roboto Mono', monospace;
