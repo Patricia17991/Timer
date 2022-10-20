2d54e98322cc1bfbb34f794d8810de7d40c3f0ca
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-20 11:02:58 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,10 +7,10 @@ export function Home() {
         <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" />
+          <TaskInput id="task"  placeholder="Dê um nome para o seu projeto" />
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" />
+          <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" />
           <span>minutos.</span>
         </FormContainer>
             
