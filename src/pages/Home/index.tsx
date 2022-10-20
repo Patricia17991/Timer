6c446d608b93741cacafd59f909aa757f187e0a2
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-20 09:45:10 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,7 +22,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton disabled type="submit">
             <Play size={24} />
             Começar
         </StartCountdownButton>
