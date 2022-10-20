042694b06ce89a5128c966f0660497e0fe680628
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-20 12:51:50 -0300

    Update index.tsx

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,30 @@
-import { HistoryContainer } from './styles'
+import { HistoryContainer, HistoryList } from './styles'
 
 export function Histort() {
     return (
-        <HistoryContainer>
-
-        </HistoryContainer>
+      <HistoryContainer>
+        <h1>Meu histórico</h1>
+   
+        <HistoryList>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Tarefa</th>
+                        <th>Duração</th>
+                        <th>Início</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Tarefa</td>
+                        <td>20 minutos</td>
+                        <td>Há 2 meses</td>
+                        <td>Concluído</td>
+                    </tr>
+                </tbody>
+            </table>
+        </HistoryList>
+      </HistoryContainer>
     )
 }
