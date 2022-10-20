a7895d8088901c1b12378b5d7f4a0e598e78fa4d
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-20 09:03:28 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,33 @@
 import { Play } from "phosphor-react";
+import { CountdownContainer, FormContainer, HomeContainer } from "./styles";
 
 export function Home() {
   return (
-    <div>
+    <HomeContainer>
         <form action="">
-        <div>
+        <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <input id="task" />
 
           <label htmlFor="minutesAmount">durante</label>
           <input type="number" id="minutesAmount" />
           <span>minutos.</span>
-        </div>
+        </FormContainer>
             
-        <div>
+        <CountdownContainer>
           <span>0</span>
           <span>0</span>
           <span>:</span>
           <span>0</span>
           <span>0</span>
-        </div>
+        </CountdownContainer>
 
         <button type="submit">
             <Play size={24} />
             Começar
         </button>
         </form>
-    </div>
+    </HomeContainer>
   )
   
 }
