b71c6bcd72f7881e91be336ccbae207d051d55e9
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-21 18:23:59 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,40 @@
 import { Play } from 'phosphor-react'
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from './styles'
+import { useState } from 'react'
+
 
 export function Home() {
+
+  const [task, setTask] = useState('')
+
   return (
     <HomeContainer>
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput 
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
+            />
 
-           <dataList id="task-suggestions">
-             <option value="Projeto 1" />
-             <option value="Projeto 2" />
-             <option value="Projeto 3" />
-             <option value="Banana" />
-           </dataList>
+         <dataList id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Banana" />
+          </dataList>
+
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput 
-           type="number" 
-           id="minutesAmount" 
-           placeholder="00"
-           step={5}
-           min={5}
-           max={60}
+            type="number" 
+            id="minutesAmount" 
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutos.</span>
         </FormContainer>
@@ -36,10 +48,10 @@ export function Home() {
         </CountdownContainer>
 
         <StartCountdownButton disabled type="submit">
-            <Play size={24} />
-            Começar
+          <Play size={24} />
+          Começar
         </StartCountdownButton>
-        </form>
+    </form>
     </HomeContainer>
   )
   
