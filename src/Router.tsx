b388e2c9c6fa1c09986829d6feb361068e823fca
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-26 09:10:15 -0300

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,13 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { History } from './pages/History'
+import { Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './layouts/DefaultLayout'
+import { History } from './pages/History'
+
+import { Home } from './pages/Home'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout/>}>
+      <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/history" element={<History />} />
       </Route>
