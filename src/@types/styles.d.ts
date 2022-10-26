b2e97629272ff60841d32236dd8a85026a0c3ecc
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-26 08:56:57 -0300

    Update styles.d.ts

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,9 +1,8 @@
-import 'styled-component';
-import {defaultTheme} from '../styles/themes/default';
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
 
-
-type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends ThemeType {}
-}//sobrescrevendo tipagens de uma bibloteca
+  export interface DefaultTheme extends ThemeType {}
+}
